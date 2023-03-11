de9e73bb40de667d3313839061f2e718467ee1bf
Aleksey <megazuzka11@gmail.com>
2023-03-11 12:11:41 +0200

    new script

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,20 @@
 'user strict';
+
+const numberOfFilm = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMuvieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i += 1) {
+    let lastWachhFilm = prompt('Один из последних фильмов?', '');
+    let gradeFilm = +prompt('Насколько оцените его?', '');
+    personalMuvieDB.movies[lastWachhFilm] = gradeFilm;
+}
+
+
+console.log(personalMuvieDB)
