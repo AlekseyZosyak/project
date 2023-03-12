7f025473e004edbf2b3dc302f52967153dcd845a
Aleksey <megazuzka11@gmail.com>
2023-03-12 02:40:08 +0200

    new bz

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,39 @@
 'user strict';
 
-const numberOfFilm = +prompt('Сколько фильмов вы посмотрели?', '');
-
 const personalMuvieDB = {
-    count: numberOfFilm,
+    count: '',
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-for(let i = 0; i < 2; i += 1) {
-    let lastWachhFilm = prompt('Один из последних фильмов?', '');
-    let gradeFilm = +prompt('Насколько оцените его?', '');
-    personalMuvieDB.movies[lastWachhFilm] = gradeFilm;
-}
+let key = true;
+let numberOfFilm = null;
+let lastWachhFilm = null;
+let gradeFilm = null;
 
+do {
+    numberOfFilm = +prompt('Сколько фильмов вы посмотрели?', '');
+    lastWachhFilm = prompt('Один из последних фильмов?', '');
+    gradeFilm = +prompt('Насколько оцените его?', '');
+
+    if(lastWachhFilm !== '' && lastWachhFilm.length === 20 || gradeFilm !== '' && numberOfFilm !== '' ) {
+        key = false;
+        personalMuvieDB['count'] = numberOfFilm;
+        personalMuvieDB.movies[lastWachhFilm] = gradeFilm;
+    } else {
+        alert('вы ввели не правильные данные, давайте еще раз');
+    }
+
+} while (key);
+
+if (personalMuvieDB.count < 10) {
+    console.log('просмотренно довольно мало фильмов');
+} else if (personalMuvieDB.count > 10 || personalMuvieDB.count < 30) {
+    console.log('классический зритель');
+} else {
+    console.log('ну вы и киноша)');
+}
 
 console.log(personalMuvieDB)
